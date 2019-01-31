38beb6ccacd7bc11bc39d56d0c1595af326d559c
Saitarun Irigala <saitarun@mail.usf.edu>
2019-01-31 18:08:10 -0500

    v1.0

--- SIrigala_Methods_Delivery/Program.cs
+++ SIrigala_Methods_Delivery/Program.cs
@@ -6,7 +6,30 @@ namespace SIrigala_Methods_Delivery
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine("Please enter your name: ");
+            string input = Console.ReadLine();
+
+            try
+            {
+                User_Name(input);
+            }//end of try
+
+            catch
+            {
+                Console.WriteLine("Please enter your name!");
+                Console.WriteLine("Press any key to exit and try again.");
+                Console.ReadKey(true);
+            }//end of catch
+            
+        }//end of main
+
+        private static void User_Name(string user_input)
+        {
+            string UserName = user_input;
+            Console.WriteLine("Hello " + UserName + ".");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey(true);
+
+        }//end of method
     }
 }
